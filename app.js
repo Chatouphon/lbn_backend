bca7930393af625f8a7a4dbf0544d86c0f1eb0a3
Daky <dkartree@gmail.com>
2021-07-05 17:18:20 +0700

    confirm step 1

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ dotenv.config();
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DB_CONNECT ,
   {
+    keepAlive: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
